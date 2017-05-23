5decea9a050ff09364e3b41a39c527cd50d87e9a
aberto <aberto20@gmail.com>
2017-05-23 09:59:29 +0200

    from abel

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,9 @@ import play.data.Form;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.bland;
+import views.html.vehicle;
 
-
-import javax.persistence.criteria.From;
-import javax.xml.transform.*;
 import java.util.List;
 
 /**
@@ -57,56 +56,7 @@ public class Application extends Controller {
         List<User> userList=User.all();
         return ok(Json.toJson(userList));
     }
-    public static Result saveUser(){
-        Form<User> userForm=Form.form(User.class).bindFromRequest();
 
-        User user=userForm.get();
-        User u=User.findByUsername(user.username);
-        if (u!=null){
-            return ok("usernameExist");
-        }
-        user.save();
-
-        List<User> userList=User.all();
-        return ok(Json.toJson(userList));
-    }
-    public static Result updateUser(){
-        Form<User> userForm=Form.form(User.class).bindFromRequest();
-        User user=userForm.get();
-
-        User user1=User.finderById(user.id);
-        user1.email=user.email;
-        user1.firstName=user.firstName;
-        user1.lastName=user.lastName;
-        user1.role=user.role;
-        user1.phone=user.phone;
-        user1.username=user.username;
-        user1.password=user.password;
-        user1.update();
-        List<User> userList=User.all();
-        return ok(Json.toJson(userList));
-    }
-    public static Result disableUser(long id){
-        Form<User> userForm=Form.form(User.class).bindFromRequest();
-        User user=userForm.get();
-        User user1=User.finderById(id);
-        user1.deleteStatus=true;
-        user1.deleteReason=user.deleteReason;
-        user1.update();
-
-        List<User> userList=User.all();
-        return ok(Json.toJson(userList));
-    }
-    public static Result enableUser(long id){
-        Form<User> userForm=Form.form(User.class).bindFromRequest();
-        User user=userForm.get();
-        User user1=User.finderById(id);
-        user1.deleteStatus=false;
-        user1.deleteReason=user.deleteReason;
-        user1.update();
-        List<User> userList=User.all();
-        return ok(Json.toJson(userList));
-    }
     public static Result blandPage(){
         if(session("userId")==null ||session("userId").equals("") ){
             return ok(views.html.login.render());
@@ -123,7 +73,7 @@ public class Application extends Controller {
         if(session("userId")==null ||session("userId").equals("") ){
             return ok(views.html.login.render());
         }
-        AbstractDocument series;
+
         return ok(views.html.series.render());
     }
     public static Result dashboarAdmin(){
--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -5,5 +5,7 @@ import play.mvc.*;
 import views.html.*;
 
 public class HomeController extends Controller {
-
+    public static Result index(){
+        return ok(index.render());
+    }
 }
