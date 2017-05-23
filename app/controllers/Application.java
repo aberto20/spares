ecc8d49ae9866b0326c380eea8f2b2250a964121
aberto20 <aberto20@gmail.com>
2017-05-23 09:29:30 +0200

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -17,6 +17,133 @@ import java.util.List;
 public class Application extends Controller {
 
 
+      public static Result loginpage(){
+        return ok(views.html.login.render());
+    }
+    public static Result logout(){
+        session().clear();
+        return ok(views.html.index.render());
+    }
+    public static Result loadCurrentUser(){
+        User user=User.findByUsername(session("userId"));
+        return ok(Json.toJson(user));
+    }
+    public static Result signin(){
+        Form<User>userForm=Form.form(User.class).bindFromRequest();
+        User user=userForm.get();
+        Boolean Auth=false;
+         session().clear();
+        for (User u:User.find.where().like("username",user.username).findList()){
+            if (u.password.equals(user.password)){
+                Auth=true;
+            }
+        }
+        if (Auth){
+            System.out.println("---------------------------------------\n Logged in !");
+            session("userId",user.username);
+            return ok("ok");
+        }else {
+            return ok("error");
+        }
+
+    }
+    public static Result adminHome(){
+        if(session("userId")==null ||session("userId").equals("") ){
+            return ok(views.html.login.render());
+        }
+        return ok(views.html.admin.render());
+    }
+    public static Result loadUsers(){
+        List<User> userList=User.all();
+        return ok(Json.toJson(userList));
+    }
+    public static Result saveUser(){
+        Form<User> userForm=Form.form(User.class).bindFromRequest();
+
+        User user=userForm.get();
+        User u=User.findByUsername(user.username);
+        if (u!=null){
+            return ok("usernameExist");
+        }
+        user.save();
+
+        List<User> userList=User.all();
+        return ok(Json.toJson(userList));
+    }
+    public static Result updateUser(){
+        Form<User> userForm=Form.form(User.class).bindFromRequest();
+        User user=userForm.get();
+
+        User user1=User.finderById(user.id);
+        user1.email=user.email;
+        user1.firstName=user.firstName;
+        user1.lastName=user.lastName;
+        user1.role=user.role;
+        user1.phone=user.phone;
+        user1.username=user.username;
+        user1.password=user.password;
+        user1.update();
+        List<User> userList=User.all();
+        return ok(Json.toJson(userList));
+    }
+    public static Result disableUser(long id){
+        Form<User> userForm=Form.form(User.class).bindFromRequest();
+        User user=userForm.get();
+        User user1=User.finderById(id);
+        user1.deleteStatus=true;
+        user1.deleteReason=user.deleteReason;
+        user1.update();
+
+        List<User> userList=User.all();
+        return ok(Json.toJson(userList));
+    }
+    public static Result enableUser(long id){
+        Form<User> userForm=Form.form(User.class).bindFromRequest();
+        User user=userForm.get();
+        User user1=User.finderById(id);
+        user1.deleteStatus=false;
+        user1.deleteReason=user.deleteReason;
+        user1.update();
+        List<User> userList=User.all();
+        return ok(Json.toJson(userList));
+    }
+    public static Result blandPage(){
+        if(session("userId")==null ||session("userId").equals("") ){
+            return ok(views.html.login.render());
+        }
+        return ok(bland.render());
+    }
+    public static Result VehiclePage(){
+        if(session("userId")==null ||session("userId").equals("") ){
+            return ok(views.html.login.render());
+        }
+        return ok(vehicle.render());
+    }
+    public static Result SeriesPage(){
+        if(session("userId")==null ||session("userId").equals("") ){
+            return ok(views.html.login.render());
+        }
+        AbstractDocument series;
+        return ok(views.html.series.render());
+    }
+    public static Result dashboarAdmin(){
+        if(session("userId")==null ||session("userId").equals("") ){
+            return ok(views.html.login.render());
+        }
+        return ok(views.html.admin.render());
+    }
+    public static Result user(){
+        if(session("userId")==null ||session("userId").equals("") ){
+            return ok(views.html.login.render());
+        }
+        return ok(views.html.user.render());
+    }
+    public static Result sparePart(){
+        if(session("userId")==null ||session("userId").equals("") ){
+            return ok(views.html.login.render());
+        }
+        return ok(views.html.sparePart.render());
+    }
 
     public static Result saveUser(){
         Form<User> userForm=Form.form(User.class).bindFromRequest();
