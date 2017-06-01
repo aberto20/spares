58b81b80a34a4b5554d1f0db5a2bfa507012d778
aberto20 <aberto20@gmail.com>
2017-06-01 15:21:54 +0200

    vfvv

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,22 +4,7 @@ var app = angular.module('spareApp', ['ngRoute']).config(function ($routeProvide
     .otherwise({redirectTo: '/'})
 
 });
-    var appinside = angular.module('spareApploggedin', ['ngRoute','angularUtils.directives.dirPagination','ngTable', 'ngTableResizableColumns']).config(function ($routeProvider) {
-    $routeProvider
-    .when('/', { templateUrl: '/assets/partials/dashboad.html' })
-    .when('/registeremployee', { templateUrl: '/viewEmployee/' })
-    .when('/children', { templateUrl: '/assets/partials/children.html' })
-    .when('/childrenInfo/:id', { templateUrl:function(params){return'/childInfo/'+params.id }})
-    .when('/ChildRegistration', { templateUrl: '/newChildRegistration/' })
-    .when('/allParent', { templateUrl: '/allParents/' })
-    .when('/allneighbour', { templateUrl: '/allNeighbours/' })
-    .when('/listConsult', { templateUrl: '/listConsultation/' })
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 61b5132d51eeb16023bcf5346f5e7ae1108be59a
-});
 
 
 
