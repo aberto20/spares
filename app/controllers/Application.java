3c631d1a34bbd29300dc25bff997ce4dc1b34731
aberto20 <aberto20@gmail.com>
2017-06-03 16:27:22 +0200

    fvfbv

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -162,6 +162,14 @@ public class Application extends Controller {
             return ok("ok");
 
     }
+    public static Result UsernameExist(String username){
+        User user=User.findByUsername(username);
+       if(user ==null){
+           return ok("error");
+       }
+        return ok("ok");
+
+    }
     public static play.mvc.Result loadBlands(){
             List<Bland> bland = Bland.all();
             return ok(Json.toJson(bland));
